4efc6e6eca723156b475c0e8b04c53144768f4a6
Victor <condicker@gmail.com>
2026-05-07 06:02:55 +0500

    Improve comments in GodObjectPerson.cpp
    Refactor comments for clarity and consistency in Person class methods.

--- GodObjectPerson.cpp
+++ GodObjectPerson.cpp
@@ -1,7 +1,5 @@
 #include "GodObjectPerson.h"
 
-
-
 Person::Person()
 {
 	
@@ -12,23 +10,22 @@ Person::~Person()
 
 }
 
-
 int Person::SetName(const std::string& name, const std::vector<std::string>& invNames)
 {
-	if (name.empty())									//if name empty - just return error int number means name is empty;
+	if (name.empty())									// if name empty - just return error int number means name is empty;
 		return nameIsEmpty;
 
-	for (const auto& worngName : invNames)				//cycle to check elements of invalidateName; auto is:	std::vector<std::string>::const_iterator::value_type = std::string; 
+	for (const auto& worngName : invNames)				// cycle to check elements of invalidateName; auto is:	std::vector<std::string>::const_iterator::value_type = std::string; 
 	{
 		if (name == worngName)
 		{
 			Person::InvalidName = worngName;
-			return invalidName;							//return error code if invalide name
+			return invalidName;							// return error code if invalide name
 		}
 				
 	}
-	Person::Name = name;								//explicit assignment of name
-	return 0;											//if all good
+	Person::Name = name;								// explicit assignment of name
+	return 0;											// if all good
 }
 
 
@@ -43,18 +40,18 @@ const std::string Person::GetGenderStr() const
 	}
 }
 
-void Person::SetRandomConditions()						//set both mental and phys RAND 										
+void Person::SetRandomConditions()						// set both mental and phys RAND 										
 {
 	TrueRandom(physConditionsMask);
 	TrueRandom(mentalConditionsMask);
 }
 
-void Person::SetRandomPhysConditions()					//set phys RAND 
+void Person::SetRandomPhysConditions()					// set phys RAND 
 {
 	TrueRandom(physConditionsMask);
 }
 
-void Person::SetRandomMentalConditions()				//set mental RAND
+void Person::SetRandomMentalConditions()				// set mental RAND
 {
 	TrueRandom(mentalConditionsMask);
 }
