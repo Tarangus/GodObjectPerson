4241711332b9a7f32dff28154962708c94738cdd
Victor <condicker@gmail.com>
2026-05-07 05:59:32 +0500

    Update author name and improve comments in header

--- GodObjectPerson.h
+++ GodObjectPerson.h
@@ -4,7 +4,7 @@
  *			But you can make a good realisation of your great class of everything; Because gods knowing everything
  *			 
  *
- * @author Victor Bersenev
+ * @author Victor Bersenev aka Tarangus
  * @date 2026-04-10
  * @version 1.0
  */
@@ -19,7 +19,7 @@ using uint = unsigned int;						//to shorten code, because this class has a lot 
 
 /*----------------------------------------------INCLUDES BLOCK----------------------------------------*/
 /*---STL---*/
-#include <string>								//good std class for work with strings
+#include <string>								//good std class to work with strings
 #include <cstdint>								//for unsigned ints 8/16/32/64
 #include <random>								//to create a template trueRandom func with good return number
 
@@ -31,26 +31,26 @@ class Person
 /*----------------------------------------------PRIVATE BLOCK-----------------------------------------*/
 private:
 	/*------------------------------------------BASIC TYPES-------------------------------------------*/
-	uint Id{};											//any object have id; {} - is default initialization.
+	uint Id{};											// any object have id; {} - is default initialization.
 	uint Age{};
 	float Height{};										
 	float Weight{};										
 
-	uint16_t physConditionsMask{};						//bitfield
+	uint16_t physConditionsMask{};						// bitfield
 	uint16_t mentalConditionsMask{};
 	
 	/*------------------------------------------COMPLEX TYPES-----------------------------------------*/
 	//---STRINGS---//
 	std::string Name;									
 	std::string Surname;								
-	std::string Patronym;								//Patronym can be empty
+	std::string Patronym;								// Patronym can be empty, be care
 	std::string NickName;								
 	std::string Race;									
 	std::string MainActivity;							
 	std::string Profession;								
 	std::string Hobbie;									
 	std::string Character;								
-	std::string InvalidName;							//single invalidate name. If you need to check some wrong names, can use this
+	std::string InvalidName;							// single invalidate name. If you need to check some wrong names, can use this
 
 	//---VECTOR STRINGS---//
 	std::vector<std::string> InvalidNamesVec;			
@@ -58,11 +58,11 @@ private:
 	std::vector<std::string> InvalidPatronyms;			
 
 	//---STRUCTS---//
-	struct FullName										//struct for Full name of person, to work with struct instead of single field and have some struct mechanic
+	struct FullName										// struct for Full name of person, to work with struct instead of single field and have some struct mechanic
 	{
 		std::string name;
 		std::string surname;
-		std::string patronym;							//can be empty, because not all cultures have patronym
+		std::string patronym;							// can be empty, because not all cultures have patronym
 	}full_name{};										
 
 
@@ -70,7 +70,7 @@ private:
 	{
 		uint day;
 		uint month;
-		int year;										//year can be negative
+		int year;										// year can be negative
 	}birth_date{};										
 
 	struct Coord3D										
@@ -78,27 +78,26 @@ private:
 		float x;
 		float y;
 		float z;
-	}coord3d{};												
+	}coord3d{};	
 
-	struct Coord2D										
+	struct Coord2D
 	{
 		float x;
 		float y;
 	}coord2d{};											
 
-	struct PhysAtt										//struct for phys attributes of person
+	struct PhysAtt										// struct for phys attributes of person
 	{
 
-	}phys_att{};										
-
-	struct MentalAtt									
+	}phys_att{};
+	struct MentalAtt
 	{
 
-	}mental_att{};										//field of mental attributes
+	}mental_att{};
 
 public:
-	Person();											
-	~Person();											
+	Person();	
+	~Person();	
 
 	/*------------------------------------------SETTERS-----------------------------------------------*/
 	/*---STRINGS---*/
@@ -116,21 +115,22 @@ public:
 	void SetInvalidName(const std::string invName)				{ InvalidName = invName; }			
 
 	/*Setters with some functional*/
-	int  SetName(const std::string& name, const std::vector<std::string>& invNames);					//setting name with check and return error code if not good, if 0 - good; it realise in GodObjectPerson.cpp file
-	void SetFullName(const std::string& name, const std::string& surname, const std::string& patronym)	//using single setters
+	int  SetName(const std::string& name, const std::vector<std::string>& invNames);					// setting name with check and return error code if not good, if 0 - good; it realise in GodObjectPerson.cpp file
+	void SetFullName(const std::string& name, const std::string& surname, const std::string& patronym)	// using single setters
 	{
 		SetName(name); SetSurname(surname); SetPatronym(patronym);
 	}
-	void SetInvalidNames(const std::vector<std::string> names)											//set vector of invalid names. income outside
+	
+	void SetInvalidNames(const std::vector<std::string> names)											// set vector of invalid names. income outside
 	{ 
 		for (auto& invName : names) InvalidNamesVec.push_back(invName); 
 	} 
 
 	/*---INTS---*/  
-	void SetID(uint id)											{ Id = id; }														
-	void SetAge(uint age)										{ Age = age; }												
-	void SetHeight(auto height)									{ Height = height; }					
-	void SetWeight(auto weight)									{ Weight = weight; }					
+	void SetID(uint id)											{ Id = id; }						
+	void SetAge(uint age)										{ Age = age; }					
+	void SetHeight(auto height)									{ Height = height; }
+	void SetWeight(auto weight)									{ Weight = weight; }
 
 	void SetPhysConditions(auto& conditionBitField) { physConditionsMask = conditionBitField; }	
 
@@ -140,7 +140,7 @@ public:
 	}
 
 	/*---STRUCTS---*/
-	void SetDateOfBirth(const uint day, const uint month, const int year)					//setting date of birth just by values
+	void SetDateOfBirth(const uint day, const uint month, const int year)								// setting date of birth just by values
 	{
 		birth_date.day = day;
 		birth_date.month = month;
@@ -151,19 +151,20 @@ public:
 	/*---STRINGS---*/
 	const std::string GetName()						const		{ return Name; }						
 	const std::string GetSurname()					const		{ return Surname; }						
-	const std::string GetPatronym()					const		{ return Patronym; }					//returning patronym (can be empty, need to check)
+	const std::string GetPatronym()					const		{ return Patronym; }					// returning patronym (can be empty, need to check)
 	const std::string GetNickname()					const		{ return NickName; }				
 	const std::string GetMainActivity()				const		{ return MainActivity; }				
 	const std::string GetRace()						const		{ return Race; }						
 	const std::string GetInvalidName()				const		{ return InvalidName; }	
-	const auto& GetInvalidNames()					const		{ return InvalidNamesVec; }				//return vector of invalid names
+	const auto& GetInvalidNames()					const		{ return InvalidNamesVec; }				// return vector of invalid names
 
 	/*---INTS---*/
 	uint GetID()									const		{ return Id; }							
 	uint GetAge()									const		{ return Age; }							
 	float GetHeight()								const		{ return Height; }						
-	float GetWeight()								const		{ return Weight; }						
-	const auto& GetPhysConditions()					const		{ return physConditionsMask; }			//returning bitfield for physConditions. auto& is used to futhurer modify bitfield
+	float GetWeight()								const		{ return Weight; }	
+	
+	const auto& GetPhysConditions()					const		{ return physConditionsMask; }			// returning bitfield for physConditions. auto& is used to futhurer modify bitfield
 	const auto& GetMentalConditions()				const		{ return mentalConditionsMask; }		
 	const auto& GetGender()							const		{ return Gender; }						
 
@@ -175,18 +176,32 @@ public:
 	/*----------------------------------------------METHODS-------------------------------------------*/
 
 
-	const std::string GetGenderStr()const;												//check .cpp file
+	const std::string GetGenderStr()const;																// check .cpp file
 
 	template<class T>
-	void TrueRandom(T& value)															//a simple template functon to get a good random number
+	void TrueRandom(T& value)																			// a simple template functon to get a good random number
 	{
-		static std::mt19937 gen(std::random_device{}());								
+		thread_local static std::mt19937 gen(std::random_device{}());								
 		uint64_t val = (static_cast<uint64_t>(gen()) << 32) | gen();
 		value = std::decay_t<T>(static_cast<T>(val));
 	}
-
+	template<typename T, typename RangeType>
+	void TrueRandom(T& value, RangeType min, RangeType max)												// Universal template func with floating
+	{
+		thread_local static std::mt19937_64 gen(std::random_device{}());
+		if constexpr (std::is_floating_point_v<T>)
+		{
+			std::uniform_real_distribution<T> dis(static_cast<T>(min), static_cast<T>(max));
+			value = dis(gen);
+		}
+		else 
+		{
+			std::uniform_int_distribution<T> dis(static_cast<T>(min), static_cast<T>(max));
+			value = dis(gen);
+		}
+	}
 	//easy to delete block, it can be useful, but yeah: additional bytes and managment;
-	void SetRandomConditions();															//setting random all conditiones for testing purposes or any other reason
+	void SetRandomConditions();																		//setting random all conditiones for testing purposes or any other reason
 	void SetRandomPhysConditions();														
 	void SetRandomMentalConditions();													
 
